6634d356bf33e412bfda2f9245f6c91893d7a049
Jaroslav Týc <mail@jaroslavtyc.com>
2023-12-12 15:40:05 +0100

    Disable deprecation errors for tests and old Symfony kernel

--- tests/Application/config/bootstrap.php
+++ tests/Application/config/bootstrap.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpKernel\Kernel;
 
 require dirname(__DIR__).'../../../vendor/autoload.php';
 
@@ -25,3 +26,8 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] ??= $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], \FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
+if (($_SERVER['APP_ENV'] ?? '') === 'test' && Kernel::MAJOR_VERSION < 6) {
+    // to avoid Behat --- Failed scenarios: 8192: Function libxml_disable_entity_loader() is deprecated in vendor/symfony/dom-crawler/Crawler.php line 1183
+    error_reporting(E_ALL ^ E_DEPRECATED);
+}
