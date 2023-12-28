10abc78ea8b06119779e89497e3e3ce035ddad9c
Jaroslav Týc <mail@jaroslavtyc.com>
2023-12-28 13:18:29 +0100

    Remove unused migrations config

--- src/DependencyInjection/ThreeBRSSyliusPaymentRestrictionExtension.php
+++ src/DependencyInjection/ThreeBRSSyliusPaymentRestrictionExtension.php
@@ -4,43 +4,17 @@ declare(strict_types=1);
 
 namespace ThreeBRS\SyliusPaymentRestrictionPlugin\DependencyInjection;
 
-use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ThreeBRSSyliusPaymentRestrictionExtension extends AbstractResourceExtension implements PrependExtensionInterface
+class ThreeBRSSyliusPaymentRestrictionExtension extends AbstractResourceExtension
 {
-    use PrependDoctrineMigrationsTrait;
-
     /** @psalm-suppress UnusedVariable */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        $this->prependDoctrineMigrations($container);
-    }
-
-    protected function getMigrationsNamespace(): string
-    {
-        return 'DoctrineMigrations';
-    }
-
-    protected function getMigrationsDirectory(): string
-    {
-        return '@ThreeBRSSyliusPaymentRestrictionPlugin/migrations';
-    }
-
-    protected function getNamespacesOfMigrationsExecutedBefore(): array
-    {
-        return [
-            'Sylius\Bundle\CoreBundle\Migrations',
-        ];
     }
 }
