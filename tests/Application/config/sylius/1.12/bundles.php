c539278183150130b9177d7ec3819a0140a822a2
Jaroslav Týc <mail@jaroslavtyc.com>
2023-12-29 10:50:41 +0100

    Fix some test app dependencies

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
