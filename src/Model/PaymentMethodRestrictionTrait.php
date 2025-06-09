f8b174f61260e745f662fcc6f377b861ed642aca
AdhamKandeel <adhamhkandeel27@gmail.com>
2025-06-09 13:22:42 +0300

    Initial commit Upgrade to sylius 2

--- src/Model/PaymentMethodRestrictionTrait.php
+++ src/Model/PaymentMethodRestrictionTrait.php
@@ -11,24 +11,17 @@ use Sylius\Component\Core\Model\ShippingMethodInterface;
 
 trait PaymentMethodRestrictionTrait
 {
-    /**
-     * @var ZoneInterface|null
-     *
-     * @ORM\ManyToOne(targetEntity="Sylius\Component\Addressing\Model\ZoneInterface")
-     */
-    private $zone;
+    #[ORM\ManyToOne(targetEntity: ZoneInterface::class)]
+    private ?ZoneInterface $zone = null;
 
-    /**
-     * @var Collection<array-key, ShippingMethodInterface>|ShippingMethodInterface[]
-     *
-     * @ORM\ManyToMany(targetEntity="Sylius\Component\Core\Model\ShippingMethod")
-     *
-     * @ORM\JoinTable(name="threebrs_payment_method_shipping_method",
-     *     joinColumns={@ORM\JoinColumn(name="payment_method_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="method_shipping_id", referencedColumnName="id")}
-     * )
-     */
-    private $shippingMethods;
+    /** @var Collection<int, ShippingMethodInterface> */
+    #[ORM\ManyToMany(targetEntity: ShippingMethodInterface::class)]
+    #[ORM\JoinTable(
+        name: 'threebrs_payment_method_shipping_method',
+        joinColumns: [new ORM\JoinColumn(name: 'payment_method_id', referencedColumnName: 'id')],
+        inverseJoinColumns: [new ORM\JoinColumn(name: 'method_shipping_id', referencedColumnName: 'id')],
+    )]
+    private Collection $shippingMethods;
 
     public function getZone(): ?ZoneInterface
     {
